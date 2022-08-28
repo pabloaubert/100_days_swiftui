41ad763a26cd9fde7ba48d26fad01d8c0b44f468
pabloaubert <pablo.aubert@me.com>
2022-08-28 12:04:01 +0200

    [ADD] Day 31

--- WorldScramble/WorldScramble/ContentView.swift
+++ WorldScramble/WorldScramble/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
   @State private var usedWords = [String]()
   @State private var rootWord = ""
   @State private var newWord = ""
+  @State private var score = 0
   
   @State private var errorTitle = ""
   @State private var errorMessage = ""
@@ -23,6 +24,7 @@ struct ContentView: View {
           TextField("Enter your word", text: $newWord)
             .autocapitalization(.none)
         }
+        Text("Score: \(score)")
         Section {
           ForEach(usedWords, id: \.self) { word in
             HStack {
@@ -32,22 +34,53 @@ struct ContentView: View {
           }
         }
       }
+      .navigationTitle(rootWord)
+      .toolbar {
+        Button("Restart") { startGame() }
+      }
     }
-    .navigationTitle(rootWord)
-    .onSubmit(addNewWord)
     .onAppear(perform: startGame)
+    .onSubmit(addNewWord)
     .alert(errorTitle, isPresented: $showingError) {
       Button("OK", role: .cancel) { }
     } message: {
       Text(errorMessage)
     }
+    
   }
   
+  // START THE GAME
+  func startGame() {
+    if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+      if let startWords = try? String(contentsOf: startWordsURL) {
+        let allWords = startWords.components(separatedBy: "\n")
+        rootWord = allWords.randomElement() ?? "silkworm"
+        usedWords = []
+        newWord = ""
+        score = 0
+        return
+      }
+    }
+    
+    fatalError("Could not load start.txt from bundle")
+  }
+  
+  // Enter new word if valid
   func addNewWord() {
     let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
     guard answer.count > 0 else { return }
     
     // Extra validation
+    guard isLong(word: answer) else {
+      wordError(title: "Word too short", message: "Sometimes size matters")
+      return
+    }
+    
+    guard isNotRoot(word: answer) else {
+      wordError(title: "Word is initial", message: "Don't be cheeky")
+      return
+    }
+    
     guard isOriginal(word: answer) else {
       wordError(title: "Word used already", message: "Be more original")
       return
@@ -63,22 +96,20 @@ struct ContentView: View {
       return
     }
     
+    addToScore(word: answer)
+    
     withAnimation {
       usedWords.insert(answer, at: 0)
     }
     newWord = ""
   }
   
-  func startGame() {
-    if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
-      if let startWors = try? String(contentsOf: startWordsURL) {
-        let allWords = startWors.components(separatedBy: "\n")
-        rootWord = allWords.randomElement() ?? "silkworm"
-        return
-      }
-    }
-    
-    fatalError("Could not load start.txt from bundle")
+  func isLong(word: String) -> Bool {
+    word.count >= 3
+  }
+  
+  func isNotRoot(word: String) -> Bool {
+    word != rootWord
   }
   
   func isOriginal(word: String) -> Bool {
@@ -104,6 +135,10 @@ struct ContentView: View {
     let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
     
     return misspelledRange.location == NSNotFound
+  }
+  
+  func addToScore(word: String) {
+    score += word.count
   }
   
   func wordError(title: String, message: String) {
@@ -191,5 +226,15 @@ struct ContentView_Previews: PreviewProvider {
  
  Use FatalError for those errors that would make the app not work at all
  
+ 
+ 
+ 
+ 
+ 
+ 
+ CHALLENGE:
+ 1) Disallow answers that are shorter than three letters or are just our start word.
+ 2) Add a toolbar button that calls startGame(), so users can restart with a new word whenever they want to.
+ 3) Put a text view somewhere so you can track and show the player’s score for a given root word. How you calculate score is down to you, but something involving number of words and their letter count would be reasonable.
  
  */
